f97adb1eda487919ee622fe00916f4078ab7e204
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-16 20:39:53 -0300

    Agrega los controladores necesarios para el CRUD de usuarios

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from '../services/users.service';
+import { CreateUserDto, UserDto } from '../dto/user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() createUserDto: UserDto) {
+    return this.usersService.updateUser(parseInt(id), createUserDto);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.deleteUser(parseInt(id));
+  }
+}
