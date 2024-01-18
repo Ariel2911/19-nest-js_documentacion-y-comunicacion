852e98df0ab620612f145fe5fda92cbaf66c1253
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-18 13:00:48 -0300

    Agrega decoradores de swagger

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,10 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ example: 'NameTest1', description: 'Nombre de usuario' })
   name: string;
+
+  @ApiProperty({ example: '99', description: 'Edad del usuario' })
   age: number;
 }
 
 export class UserDto {
+  @ApiProperty({
+    example: '1',
+    description: 'Número agregado de manera automática',
+  })
   id: number;
+
+  @ApiProperty({ example: 'NameTest1', description: 'Nombre de usuario' })
   name: string;
+
+  @ApiProperty({ example: '99', description: 'Edad del usuario' })
   age: number;
 }
