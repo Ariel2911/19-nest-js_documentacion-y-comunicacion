613c90ebbe96b9034d29ff0b1a6f0b505397f59f
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-18 21:18:55 -0300

    Agrega comentarios en formato JSDoc

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,23 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto, UserDto } from '../dto/user.dto';
+
 let UserId = 0;
 
 @Injectable()
 export class UsersService {
   users: UserDto[] = [];
-
+  /**
+   *
+   * @param newUser
+   * @returns Si los datos son correctos retorna los datos del usuario creado. Si son incorrectos retorna el mensaje: Datos incorrectos.
+   */
   createUser(newUser: CreateUserDto): UserDto {
-    const user = { ...newUser, id: ++UserId };
+    if (newUser.hasOwnProperty('name') && newUser.hasOwnProperty('age')) {
+      const user = { ...newUser, id: ++UserId };
 
-    this.users.push(user);
+      this.users.push(user);
 
-    return user;
+      return user;
+    } else {
+      throw new Error('Datos incorrectos');
+    }
   }
 
   getUsers(): UserDto[] {
     return this.users;
   }
-
+  /**
+   *
+   * @param id
+   * @param userToUpdate
+   * @returns mensaje informando si la actualización fue exitosa o no
+   */
   updateUser(id: number, userToUpdate: UserDto): string {
     const indexUserToUpdate = this.users.findIndex((user) => user.id === id);
 
@@ -36,6 +50,11 @@ export class UsersService {
     return 'Usuario no encontardo';
   }
 
+  /**
+   *
+   * @param id
+   * @returns un mensaje espesificando si el usuario pudo ser eliminado
+   */
   deleteUser(id: number): string {
     const indexUserToDelete = this.users.findIndex((user) => user.id === id);
 
